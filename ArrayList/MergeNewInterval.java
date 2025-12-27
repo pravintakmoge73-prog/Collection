6777d31ba5732ff726c91c7530b284b9bcbf1d0c
Pravin Takmoge <pravintakmoge73@gmail.com>
2025-12-27 12:29:44 +0530

    Interval Merged

--- ArrayList/MergeNewInterval.java
+++ ArrayList/MergeNewInterval.java
@@ -18,7 +18,7 @@ public class MergeNewInterval
 		System.out.println("Enter Number of Intervals To be Stored :");
 		int n=sc.nextInt();
 		
-		System.out.println("Enter Intervals :");
+		System.out.println("Insert interval into a sorted list and non-overlapping :");
 		for(int i=0; i<n; i++)
 		{
 			System.out.println("Enter start And end of Intervals :");
@@ -28,14 +28,90 @@ public class MergeNewInterval
 		}
 		
 		System.out.println("Intervals are :");
-		for(int i=0; i<n; i++)
+		/*for(int i=0; i<n; i++)
 		{
 			System.out.print(Arrays.toString(intr.get(i))+" ");
-		}
+		}*/
 		
 		for(int[] elements :intr)
 		{
 			System.out.println("["+elements[0]+", "+elements[1]+"]");
 		}
+		System.out.println("Enter intervals do you wants to add :");
+		//int m=sc.nextInt();
+		
+		for(int i=0; i<1; i++)
+		{
+			System.out.println("Enter start and end of Intervals :");
+			int start =sc.nextInt();
+			int end = sc.nextInt();
+			intr.add(new int[]{start, end});
+		}
+		
+		ArrayList<int[]> intr1 = new ArrayList<>();
+		
+		int[] secLastEle = intr.get(intr.size()-2);
+		int[] lastEle = intr.get(intr.size()-1);
+		
+		if(secLastEle[0]>lastEle[0])
+		{
+			intr1 = sortIntervals(intr);
+		}
+		System.out.println("Intrvals After Merging :");
+		for(int[] ele : intr1)
+		{
+			System.out.print("["+ele[0]+", "+ele[1]+"] ");
+		}
+	}
+		
+		//now sort the Intervals
+	static ArrayList<int[]> sortIntervals(ArrayList<int[]> intr)
+	{
+		for(int i=0; i<intr.size(); i++)
+		{
+			for(int j=i+1; j<intr.size(); j++)
+			{
+				if(intr.get(i)[0] > intr.get(j)[0])
+				{
+					int[] temp = intr.get(i);
+					intr.set(i, intr.get(j));
+					intr.set(j, temp);
+				}
+			}
+		}
+			System.out.println("ok");
+
+		
+		System.out.println("Intervals After sorting :");
+		
+		for(int[] elements :intr)
+		{
+			System.out.println("["+elements[0]+", "+elements[1]+"]");
+		}
+		ArrayList<int[]> intr1=new ArrayList<>();
+		intr1.add(intr.get(0));
+		
+		for(int j=0; j<intr.size(); j++)
+		{
+			int[] last = intr1.get(intr1.size()-1);
+			int[] current = intr.get(j);
+			if(last[1]>=current[0])
+			{
+				if(last[1]>current[0])
+				{
+					last[1] = last[1];
+				}
+				else
+				{
+					last[1] = current[0];
+				}
+			}
+			else
+			{
+				intr1.add(current);
+			}
+			
+		}
+		return intr1;
 	}
 }
