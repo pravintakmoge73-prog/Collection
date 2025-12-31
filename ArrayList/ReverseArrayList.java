a01deeee9a3723b738f459063e894838a0a1cc41
Pravin Takmoge <pravintakmoge73@gmail.com>
2025-12-31 22:18:44 +0530

    Revesed ArrayList

--- ArrayList/ReverseArrayList.java
+++ ArrayList/ReverseArrayList.java
@@ -12,7 +12,7 @@ public class ReverseArrayList
 	public static void main(String[] args)
 	{
 		Scanner sc = new Scanner(System.in);
-		
+
 		ArrayList<Integer> al = new ArrayList<>();
 		al.add(1);
 		al.add(2);
@@ -20,16 +20,16 @@ public class ReverseArrayList
 		al.add(4);
 		al.add(5);
 		al.add(6);
-		
+
 		System.out.println("Original ArrayList : "+al);
-		
+
 		for(int i=0; i<(al.size()/2); i++)
 		{
 			int temp = al.get(i);
 			al.set(i, al.get(al.size()-i-1));
 			al.set((al.size()-i-1), temp);
 		}
-		
+
 		System.out.println("ArrayList After Reverse :"+al);
 	}
 }
